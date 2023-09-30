70befe0accdf859c81614f07aab5ed5b8f7c23ff
ChristianMazzaro <christianmazzaro2@gmail.com>
2023-09-30 13:05:32 +0200

    esercizio vue router completato

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -15,4 +15,16 @@ class ProjectController extends Controller
         $projects = Project::paginate(3);
         return response()->json($projects);
     }
+
+    public function show(string $id)
+    {
+        $project = Project::find($id);
+    
+        if (!$project) {
+            return response()->json(['error' => 'Progetto non trovato'], 404);
+        }
+    
+        return response()->json($project);
+    }
+    
 }
